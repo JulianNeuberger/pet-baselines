eaf6349f29488f01f7d56c15147a7e6f863301a7
BennyEng <Benedikt.Engelmann@uni-bayreuth.de>
2023-06-19 11:06:16 +0200

    fixed paths

--- experiments_trafo/all_experiments.py
+++ experiments_trafo/all_experiments.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def get_scores_unaugmented():
-    train_folds = [data.loader.read_documents_from_json(f'../jsonl/fold_{i}/train.json') for i in range(5)]
-    test_folds = [data.loader.read_documents_from_json(f'../jsonl/fold_{i}/test.json') for i in range(5)]
+    train_folds = [data.loader.read_documents_from_json(f'jsonl/fold_{i}/train.json') for i in range(5)]
+    test_folds = [data.loader.read_documents_from_json(f'jsonl/fold_{i}/test.json') for i in range(5)]
     names = ["all_means", "ttr", "ucer", "ttr_mean", "ucer_mean"]
 
     # specific for this experiment
@@ -26,18 +26,18 @@ def get_scores_unaugmented():
                                           aug_train_folds=train_folds, f_score=f_1_score)
 
     df_complete = df_complete.append(all_scores[0], ignore_index=True)
-    df_complete.to_json(path_or_buf=f"./../experiment_results/unaugmented/{names[0]}.json", indent=4)
+    df_complete.to_json(path_or_buf=f"./experiment_results/unaugmented/{names[0]}.json", indent=4)
 
     # create jsons
     for k in range(1, len(all_scores)):
         df = all_scores[k]
-        df.to_json(path_or_buf=f"./../experiment_results/unaugmented/{names[k]}.json", indent=4)
+        df.to_json(path_or_buf=f"./experiment_results/unaugmented/{names[k]}.json", indent=4)
 
 
 def experiment1_1():
     # Get the data for augmenting and training
-    train_folds = [data.loader.read_documents_from_json(f'../jsonl/fold_{i}/train.json') for i in range(5)]
-    test_folds = [data.loader.read_documents_from_json(f'../jsonl/fold_{i}/test.json') for i in range(5)]
+    train_folds = [data.loader.read_documents_from_json(f'jsonl/fold_{i}/train.json') for i in range(5)]
+    test_folds = [data.loader.read_documents_from_json(f'jsonl/fold_{i}/test.json') for i in range(5)]
     names = ["all_means", "ttr", "ucer", "ttr_mean", "ucer_mean", "bleu"]
 
     # specific for this experiment
@@ -65,10 +65,10 @@ def experiment1_1():
 
         for k in range(1, len(all_scores) - 1):
             df = all_scores[k]
-            df.to_json(path_or_buf=f"./../experiment_results/trafo3/exp3.3/{names[k]}_{i/5}.json", indent=4)
+            df.to_json(path_or_buf=f"./experiment_results/trafo3/exp3.3/{names[k]}_{i/5}.json", indent=4)
 
     df_complete.index = ["0.2", "0.4", "0.6", "0.8", "1.0"]
-    df_complete.to_json(path_or_buf=f"./../experiment_results/trafo3/exp3.3/{names[0]}.json", indent=4)
+    df_complete.to_json(path_or_buf=f"./experiment_results/trafo3/exp3.3/{names[0]}.json", indent=4)
     sns.set_theme()
     df_complete.plot(kind="bar")
     plt.show()
