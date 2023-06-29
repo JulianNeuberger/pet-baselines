d9899ae234f24105ecc61f74cadd8b6e9523baba
BennyEng <Benedikt.Engelmann@uni-bayreuth.de>
2023-06-29 11:16:52 +0200

    run unaug

--- experiments_trafo/all_experiments.py
+++ experiments_trafo/all_experiments.py
@@ -7,6 +7,40 @@ import augment
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def get_unaug():  # Probability of replacement
+    # Get the data for augmenting and training
+    train_folds = [data.loader.read_documents_from_json(f'jsonl/fold_{i}/train.json') for i in range(5)]
+    test_folds = [data.loader.read_documents_from_json(f'jsonl/fold_{i}/test.json') for i in range(5)]
+    names = ["all_means", "ttr", "ucer", "ttr_mean", "ucer_mean", "bert"]
+    doubled_train_folds = []
+    # specific for this experiment
+    df_complete: pd.DataFrame = pd.DataFrame(columns=['$F_{1}_CRF$', '$F_{1}_Neural$', '$F_{1}_Relation$', '$TTR$', '$UCER$', '$BertScore$'])
+    df_entities = pd.DataFrame(columns=['Actor', 'Activity', 'Activity Data', 'Further Specification', 'XOR Gateway',
+                                        'Condition Specification', 'AND Gateway'])
+
+
+    # actual augmentation
+    for j in range(5):
+        train_fold = copy.deepcopy(train_folds[j])
+        train_fold.extend(train_folds[j])
+        doubled_train_folds.append(train_fold)
+
+    # actual training
+    f_1_scores = run_experiment("Experiment 101.1", doubled_train_folds, test_folds)
+    df_entities = df_entities.append(f_1_scores[3], ignore_index=True)
+    # evaluation
+    all_scores = evaluate_experiment_bert(unaug_train_folds=doubled_train_folds,
+                                          aug_train_folds=doubled_train_folds, f_score_crf=f_1_scores[0],
+                                          f_score_neural=f_1_scores[1],
+                                          f_score_rel=f_1_scores[2])
+
+    df_complete = df_complete.append(all_scores[0], ignore_index=True)
+    for k in range(1, len(all_scores) - 1):
+        df = all_scores[k]
+        df.to_json(path_or_buf=f"./experiment_results/unaug.json", indent=4)
+
+    df_entities.to_json(path_or_buf="./experiment_results/all_entities_f1_unaug.json", indent=4)
+    df_complete.to_json(path_or_buf=f"./experiment_results/trafo101/exp101.1/all_means_unaug.json", indent=4)
 
 def get_scores_unaugmented():
     train_folds = [data.loader.read_documents_from_json(f'jsonl/fold_{i}/train.json') for i in range(5)]
@@ -34,43 +68,6 @@ def get_scores_unaugmented():
     for k in range(1, len(all_scores)):
         df = all_scores[k]
         df.to_json(path_or_buf=f"./experiment_results/unaugmented/{names[k]}.json", indent=4)
-
-
-def experiment1_1():
-    # Get the data for augmenting and training
-    train_folds = [data.loader.read_documents_from_json(f'jsonl/fold_{i}/train.json') for i in range(5)]
-    test_folds = [data.loader.read_documents_from_json(f'jsonl/fold_{i}/test.json') for i in range(5)]
-    names = ["all_means", "ttr", "ucer", "ttr_mean", "ucer_mean", "bleu"]
-
-    # specific for this experiment
-    df_complete: pd.DataFrame = pd.DataFrame(columns=['$F_{1}_CRF$', '$TTR$', '$UCER$', '$BleuScore$'])
-
-    # augment the dataset - for i in range of the parameter
-    for i in range(1, 6):
-        augmented_train_folds = copy.deepcopy(train_folds)
-        augmentation_step: augment.AugmentationStep = augment.Trafo3Step(prob=i/5)  # adapt
-
-        # actual augmentation
-        for j in range(5):
-            augmented_train_set = augment.run_augmentation(augmented_train_folds[j], augmentation_step)
-            augmented_train_set.extend(train_folds[j])
-            augmented_train_folds[j] = copy.deepcopy(augmented_train_set)
-
-        # actual training
-        f_1_score = run_experiment("Experiment 1.1", augmented_train_folds, test_folds)
-
-        # evaluation
-        all_scores = evaluate_experiment_bleu(unaug_train_folds=train_folds,
-                            aug_train_folds=augmented_train_folds, f_score=f_1_score)
-
-        df_complete = df_complete.append(all_scores[0], ignore_index=True)
-
-        for k in range(1, len(all_scores) - 1):
-            df = all_scores[k]
-            df.to_json(path_or_buf=f"./experiment_results/trafo3/exp3.3/{names[k]}_{i/5}.json", indent=4)
-
-    df_complete.index = ["0.2", "0.4", "0.6", "0.8", "1.0"]
-    df_complete.to_json(path_or_buf=f"./experiment_results/trafo3/exp3.3/{names[0]}.json", indent=4)
 
 
 def experiment101_1():  # Probability of replacement
@@ -1850,13 +1847,13 @@ def experiment40_4():
 #experiment33_1()
 #experiment101_1() #
 #experiment101_2() #
-experiment58_1()
-experiment58_2()
-experiment5_1()
-experiment82_1()
-experiment82_2()
-experiment100_1()
-experiment100_2()
+#experiment58_1()
+#experiment58_2()
+#experiment5_1()
+#experiment82_1()
+#experiment82_2()
+#experiment100_1()
+#experiment100_2()
 #experiment90_1() #
 #experiment103_1() #
 #experiment103_2() #
@@ -1873,3 +1870,4 @@ experiment100_2()
 #experiment19_1()
 #experiment19_2()
 #experiment19_3()
+get_unaug()
--- experiments_trafo/plot_diagrams.py
+++ experiments_trafo/plot_diagrams.py
@@ -22,39 +22,176 @@ class Plot:
         ucer = []
         bleu = []
         for i in range(1, 21):
-            ttr.append([df["$F_{1}$"][i - 1], df["$TTR$"][i - 1], f"{i/20}"])
-            ucer.append([df["$F_{1}$"][i - 1], df["$UCER$"][i - 1], f"{i/20}"])
+            ttr.append([df["$F_{1}_CRF$"][i - 1], df["$TTR$"][i - 1], f"{i/20}"])
+            ucer.append([df["$F_{1}_CRF$"][i - 1], df["$UCER$"][i - 1], f"{i/20}"])
         df_ttr = pd.DataFrame(ttr, columns=['F1 Score', 'TTR', 'Prob'])
         df_ucer = pd.DataFrame(ucer, columns=['F1 Score', 'UCER', 'Prob'])
         #fig = sns.lmplot(x='TTR', y='F1 Score', data=df_ttr,  palette=palette, hue='Prob', fit_reg=True,)
-        fig = sns.lineplot(x='$TTR$', y='$F_{1}$', data=df2,  palette=palette)
+        fig = sns.lineplot(x='$TTR$', y='$F_{1}_CRF$', data=df2,  palette=palette)
         #sns.lmplot(x='UCER', y='F1 Score', data=df_ucer, fit_reg=True)
         #fig.savefig("./../experiment_results/trafo3/exp3.1/all_means.png")
         #fig.savefig("./../experiment_results/trafo3/exp3.1/all_means.pdf")
-df2 = pd.DataFrame([[0.6, 0.4], [0.65, 0.45], [0.7, 0.6],
 
-                   [0.75, 0.65], [0.9, 0.75]],
+    @staticmethod
+    def all_means_prob_bert():
+        sns.set_theme()
+        str = "101"
+        str2 = "101.1"
+        path = f"./../experiment_results/trafo{str}/exp{str2}/all_means.json"
+        df = pd.read_json(path_or_buf=path)
 
-                  columns=['F1 Score', 'TTR'])
-path = "./../experiment_results/trafo3/exp3.1/all_means.json"
-columns=['F1 Score', 'TTR']
+        prob_list = ["0.05", "0.1", "0.15", "0.2", "0.25", "0.3", "0.35", "0.4", "0.45", "0.5", "0.55", "0.6",
+                     "0.65", "0.7", "0.75", "0.8", "0.85", "0.9", "0.95", "1.0"]
 
-prob = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-f1 = [0.58, 0.6, 0.615, 0.592, 0.55, 0.547, 0.535, 0.534, 0.53, 0.52]
-bleu = [0.89, 0.875, 0.87, 0.865, 0.86, 0.855, 0.854, 0.85, 0.849, 0.84]
-bert = [0.8, 0.809, 0.83, 0.84, 0.8456, 0.86, 0.87345, 0.88, 0.889, 0.9]
-ttr = [0.20, 0.22, 0.23, 0.25, 0.27, 0.28, 0.29, 0.3, 0.315, 0.32]
-df4 = pd.DataFrame()
-sns.set_theme()
-df4["F1"] = f1
-df4["Probability"] = prob
-df4["Bleu Score"] = bleu
-df4["TTR"] = ttr
-palette = sns.color_palette("flare", n_colors=10)
-fig = sns.lineplot(x='Bleu Score', y='F1', data=df4)
-figg = fig.figure
-figg.savefig("./../experiment_results/trafo3/exp3.1/test.png")
-#Plot.plot_scatter_with_regression(path)
-plt.show()
-#Plot.plot_scatter_with_regression(path)
-plt.show()
+        df["Probability"] = prob_list
+        fig1 = sns.lineplot(x='Probability', y='F1 CRF', data=df)
+        figg1 = fig1.figure
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_prob_f1.png")
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_prob_f1.pdf")
+        plt.figure()
+
+        fig2 = sns.lineplot(x='TTR', y='F1 CRF', data=df)
+        figg2 = fig2.figure
+        figg2.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ttr_f1.png")
+        figg2.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ttr_f1.pdf")
+        plt.figure()
+
+        fig3 = sns.lineplot(x='Probability', y='TTR', data=df)
+        figg3 = fig3.figure
+        figg3.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ttr_prob.png")
+        figg3.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ttr_prob.pdf")
+        plt.figure()
+
+        fig4 = sns.lineplot(x='UCER', y='F1 CRF', data=df)
+        figg4 = fig4.figure
+        figg4.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ucer_f1.png")
+        figg4.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ucer_f1.pdf")
+        plt.figure()
+
+        fig5 = sns.lineplot(x='Probability', y='UCER', data=df)
+        figg5 = fig5.figure
+        figg5.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ucer_prob.png")
+        figg5.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ucer_prob.pdf")
+        plt.figure()
+
+        fig6 = sns.lineplot(x='BertScore', y='F1 CRF', data=df)
+        figg6 = fig6.figure
+        figg6.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_bert_f1.png")
+        figg6.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_bert_f1.pdf")
+        plt.figure()
+
+        fig7 = sns.lineplot(x='Probability', y='BertScore', data=df)
+        figg7 = fig7.figure
+        figg7.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_bert_prob.png")
+        figg7.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_bert_prob.pdf")
+        plt.figure()
+    @staticmethod
+    def all_means_prob_bleu():
+        sns.set_theme()
+        str = "3"
+        str2 = "3.1"
+        path = f"./../experiment_results/trafo{str}/exp{str2}/all_means.json"
+        df = pd.read_json(path_or_buf=path)
+
+        prob_list = ["0.05", "0.1", "0.15", "0.2", "0.25", "0.3", "0.35", "0.4", "0.45", "0.5", "0.55", "0.6",
+                     "0.65", "0.7", "0.75", "0.8", "0.85", "0.9", "0.95", "1.0"]
+
+        df["Probability"] = prob_list
+
+        fig1 = sns.lineplot(x='Probability', y='F1 CRF', data=df)
+        figg1 = fig1.figure
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_prob_f1.png")
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_prob_f1.pdf")
+        plt.figure()
+        fig2 = sns.lineplot(x='TTR', y='F1 CRF', data=df)
+        figg2 = fig2.figure
+        figg2.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ttr_f1.png")
+        figg2.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ttr_f1.pdf")
+        plt.figure()
+        fig3 = sns.lineplot(x='Probability', y='TTR', data=df)
+        figg3 = fig3.figure
+        figg3.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ttr_prob.png")
+        figg3.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ttr_prob.pdf")
+        plt.figure()
+        fig4 = sns.lineplot(x='UCER', y='F1 CRF', data=df)
+        figg4 = fig4.figure
+        figg4.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ucer_f1.png")
+        figg4.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ucer_f1.pdf")
+        plt.figure()
+        fig5 = sns.lineplot(x='Probability', y='UCER', data=df)
+        figg5 = fig5.figure
+        figg5.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ucer_prob.png")
+        figg5.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_ucer_prob.pdf")
+        plt.figure()
+        fig6 = sns.lineplot(x='BleuScore', y='F1 CRF', data=df)
+        figg6 = fig6.figure
+        figg6.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_bleu_f1.png")
+        figg6.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_bleu_f1.pdf")
+        plt.figure()
+        fig7 = sns.lineplot(x='Probability', y='BleuScore', data=df)
+        figg7 = fig7.figure
+        figg7.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_bleu_prob.png")
+        figg7.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_means_bleu_prob.pdf")
+        plt.figure()
+
+
+    @staticmethod
+    def all_entities_prob():
+        sns.set_theme()
+        str = "3"
+        str2 = "3.1"
+        path = f"./../experiment_results/trafo{str}/exp{str2}/all_entities_f1.json"
+        df = pd.read_json(path_or_buf=path)
+
+        prob_list = ["0.05", "0.1", "0.15", "0.2", "0.25", "0.3", "0.35", "0.4", "0.45", "0.5", "0.55", "0.6",
+                     "0.65", "0.7", "0.75", "0.8", "0.85", "0.9", "0.95", "1.0"]
+
+        df["Probability"] = prob_list
+
+        fig1 = sns.lineplot(x='Probability', y='Actor', data=df)
+        figg1 = fig1.figure
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_actor.png")
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_actor.pdf")
+        plt.figure()
+
+        fig1 = sns.lineplot(x='Probability', y='Activity', data=df)
+        figg1 = fig1.figure
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_activity.png")
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_activity.pdf")
+        plt.figure()
+
+        fig1 = sns.lineplot(x='Probability', y='Activity Data', data=df)
+        figg1 = fig1.figure
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_actdata.png")
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_actdata.pdf")
+        plt.figure()
+
+        fig1 = sns.lineplot(x='Probability', y='Further Specification', data=df)
+        figg1 = fig1.figure
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_further.png")
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_further.pdf")
+        plt.figure()
+
+        fig1 = sns.lineplot(x='Probability', y='XOR Gateway', data=df)
+        figg1 = fig1.figure
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_xor.png")
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_xor.pdf")
+        plt.figure()
+
+        fig1 = sns.lineplot(x='Probability', y='Condition Specification', data=df)
+        figg1 = fig1.figure
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_cond.png")
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_cond.pdf")
+        plt.figure()
+
+        fig1 = sns.lineplot(x='Probability', y='AND Gateway', data=df)
+        figg1 = fig1.figure
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_and.png")
+        figg1.savefig(f"./../experiment_results/trafo{str}/exp{str2}/plots/all_ent_prob_and.pdf")
+        plt.figure()
+
+
+
+
+Plot.all_means_prob_bleu()
+Plot.all_entities_prob()
