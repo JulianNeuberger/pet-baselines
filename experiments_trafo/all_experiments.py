a4985c239ef291ed88ec5009dcf8a74303497c68
BennyEng <Benedikt.Engelmann@uni-bayreuth.de>
2023-07-01 15:29:26 +0200

    run 5

--- experiments_trafo/all_experiments.py
+++ experiments_trafo/all_experiments.py
@@ -4431,8 +4431,8 @@ def experiment103test():  # with rate
 #experiment86rate025() #
 #experiment82rate025() #
 #experiment90rate025() #
-experiment100rate025()
-#experiment5rate025()
+#experiment100rate025() #
+experiment5rate025() #
 #experiment39rate025()
 #experiment40rate025()
 #experiment103rate025()
