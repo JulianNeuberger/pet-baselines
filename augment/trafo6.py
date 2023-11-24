bcab9c88de138c44806022bd52914c56b13fa223
Julian Neuberger <neuberger.julian@gmail.com>
2023-11-24 17:19:01 +0100

    makes trafo B.6 use number of changes instead of probability

--- augment/trafo6.py
+++ augment/trafo6.py
@@ -1,6 +1,6 @@
 import copy
 import typing
-from random import random
+import random
 
 import spacy
 
@@ -10,32 +10,43 @@ from transformations import tokenmanager
 
 
 class Trafo6Step(base.AugmentationStep):
-    def __init__(self, dataset: typing.List[model.Document], p=0.5):
+    def __init__(self, dataset: typing.List[model.Document], n=1):
         super().__init__(dataset)
-        self.p = p
+        self.n = n
         self.nlp = spacy.load("en_core_web_sm")
 
     @staticmethod
     def get_params() -> typing.List[typing.Union[params.Param]]:
         return [
-            params.FloatParam(name="p", min_value=0.0, max_value=1.0),
+            params.IntegerParam(name="n", min_value=1, max_value=20),
         ]
 
     def do_augment(self, doc: model.Document) -> model.Document:
         doc = doc.copy()
+        candidates: typing.List[model.Token] = []
         for sentence in doc.sentences:
             for token in sentence.tokens:
-                if random() >= self.p:
-                    continue
-
                 if token.text.lower() == "not":
-                    tokenmanager.delete_token(doc, token.index_in_document)
-                    continue
+                    candidates.append(token)
 
                 spacy_document = self.nlp(token.text)
                 if len(spacy_document) <= 1:
                     continue
 
                 if spacy_document[1].text == "n't":
-                    token.text = spacy_document[0].text
+                    candidates.append(token)
+
+        random.shuffle(candidates)
+        for token in candidates[:self.n]:
+            if token.text.lower() == "not":
+                tokenmanager.delete_token(doc, token.index_in_document)
+                continue
+
+            spacy_document = self.nlp(token.text)
+            if len(spacy_document) <= 1:
+                continue
+
+            if spacy_document[1].text == "n't":
+                token.text = spacy_document[0].text
+
         return doc
