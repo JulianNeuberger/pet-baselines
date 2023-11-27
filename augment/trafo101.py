b97e691e593a9ae0efd56cbb6e542da72b5d689d
Julian Neuberger <neuberger.julian@gmail.com>
2023-11-27 16:01:33 +0100

    reworks B.101 to use new substitution base class

--- augment/trafo101.py
+++ augment/trafo101.py
@@ -1,56 +1,67 @@
-from random import random
-
 import typing
+import random
+
+from nltk.corpus import wordnet
 
 from augment import base, params
-from nltk.corpus import wordnet
 from data import model
-import numpy as np
 from pos_enum import Pos
 
 
-# Author: Benedikt
-class Trafo101Step(base.AugmentationStep):
-    name = "101"
-
+class Trafo101Step(base.BaseTokenReplacementStep):
     def __init__(
         self,
         dataset: typing.List[model.Document],
         prob: float = 1,
-        tag_groups: typing.List[Pos] = None,
-        no_dupl=False,
+        n: int = 10,
     ):
-        super().__init__(dataset)
+        super().__init__(dataset, n)
         self.prob = prob
-        self.pos_tags_to_consider: typing.List[str] = [
-            v for group in tag_groups for v in group.tags
-        ]
-        self.no_dupl = no_dupl
+        self.relevant_pos = Pos.VERB.tags + Pos.AD.tags + Pos.NOUN.tags
+
+    @staticmethod
+    def get_wordnet_pos(treebank_tag: str):
+        if treebank_tag.startswith("J"):
+            return wordnet.ADJ
+        elif treebank_tag.startswith("V"):
+            return wordnet.VERB
+        elif treebank_tag.startswith("N"):
+            return wordnet.NOUN
+        elif treebank_tag.startswith("R"):
+            return wordnet.ADV
+        else:
+            return ""
 
     @staticmethod
     def get_params() -> typing.List[typing.Union[params.Param]]:
-        return [
-            params.FloatParam(name="prob", min_value=0.0, max_value=1.0),
-            params.ChoiceParam(name="tag_groups", choices=list(Pos), max_num_picks=4),
-            params.BooleanParameter(name="no_dupl"),
-        ]
+        return [params.FloatParam(name="prob", min_value=0.0, max_value=1.0)]
 
-    def do_augment(self, doc: model.Document) -> model.Document:
-        doc = doc.copy()
-        changed_words = []
-        for sentence in doc.sentences:
-            for token in sentence.tokens:
-                exists = False
-                if token.pos_tag in self.pos_tags_to_consider and random() < self.prob:
-                    if self.no_dupl is True and (token.text in changed_words):
-                        exists = True
-                    if not exists:
-                        syns = wordnet.synsets(token.text)
-                        syns = [syn.name().split(".")[0] for syn in syns]
-                        for i in range(len(syns)):
-                            if token.text != syns[i]:
-                                changed_words.append(token.text)
-                                token.text = syns[i]
+    def get_replacement_candidates(
+        self, doc: model.Document
+    ) -> typing.List[typing.List[model.Token]]:
+        candidates = []
 
-                                break
-        return doc
+        for token in doc.tokens:
+            if token.pos_tag not in self.relevant_pos:
+                continue
+            candidates.append([token])
+
+        return candidates
+
+    def get_replacement(
+        self, candidate: typing.List[model.Token]
+    ) -> typing.Optional[typing.List[str]]:
+        assert len(candidate) == 1
+        token = candidate[0]
+        text = candidate[0].text
+        synsets = wordnet.synsets(text, pos=self.get_wordnet_pos(token.pos_tag))
+        synsets = [s.name().split(".")[0] for s in synsets]
+        synsets = [s for s in synsets if s.lower() != text]
+        synsets = list(set(synsets))
+        if len(synsets) == 0:
+            return None
+        if random.random() >= self.prob:
+            return None
+        synonym = random.choice(synsets)
+        synonym = synonym.replace("_", " ")
+        return synonym.split()
--- tests/test_trafo101.py
+++ tests/test_trafo101.py
@@ -1,82 +1,88 @@
 import copy
 
-import pytest
-from augment.trafo101 import Trafo101Step
+from augment import trafo101
 from data import model
-import mock
 
-# Author for entire script: Benedikt
+
 def test_do_augment():
     # Arrange
-    trafo = Trafo101Step(type=True, no_dupl=False)
-    trafo2 = Trafo101Step(type=False, no_dupl=False)
-    trafo3 = Trafo101Step(type=True, no_dupl=True)
-    tokens = []
-    tokens.append(model.Token(text="I", index_in_document=0,
-                              pos_tag="PRP", bio_tag="",
-                              sentence_index=0))
-    tokens.append(model.Token(text="am", index_in_document=1,
-                              pos_tag="VBP", bio_tag="",
-                              sentence_index=0))
-    tokens.append(model.Token(text="the", index_in_document=2,
-                              pos_tag="DT", bio_tag="",
-                              sentence_index=0))
-    tokens.append(model.Token(text="worker", index_in_document=3,
-                              pos_tag="NN", bio_tag="",
-                              sentence_index=0))
-    tokens.append(model.Token(text="of", index_in_document=4,
-                              pos_tag="IN", bio_tag="",
-                              sentence_index=0))
-    tokens.append(model.Token(text="the", index_in_document=5,
-                              pos_tag="DT", bio_tag="",
-                              sentence_index=0))
-    tokens.append(model.Token(text="functional", index_in_document=6,
-                              pos_tag="JJ", bio_tag="",
-                              sentence_index=0))
-    tokens.append(model.Token(text="department", index_in_document=7,
-                              pos_tag="NN", bio_tag="",
-                              sentence_index=0))
-    tokens.append(model.Token(text=".", index_in_document=8,
-                              pos_tag=".", bio_tag="",
-                              sentence_index=0))
-    sentence1 = model.Sentence(tokens=tokens)
+
+    tokens = [
+        model.Token(
+            text="I", index_in_document=0, pos_tag="PRP", bio_tag="", sentence_index=0
+        ),
+        model.Token(
+            text="am", index_in_document=1, pos_tag="VBP", bio_tag="", sentence_index=0
+        ),
+        model.Token(
+            text="the", index_in_document=2, pos_tag="DT", bio_tag="", sentence_index=0
+        ),
+        model.Token(
+            text="worker",
+            index_in_document=3,
+            pos_tag="NN",
+            bio_tag="",
+            sentence_index=0,
+        ),
+        model.Token(
+            text="of", index_in_document=4, pos_tag="IN", bio_tag="", sentence_index=0
+        ),
+        model.Token(
+            text="the", index_in_document=5, pos_tag="DT", bio_tag="", sentence_index=0
+        ),
+        model.Token(
+            text="functional",
+            index_in_document=6,
+            pos_tag="JJ",
+            bio_tag="",
+            sentence_index=0,
+        ),
+        model.Token(
+            text="department",
+            index_in_document=7,
+            pos_tag="NN",
+            bio_tag="",
+            sentence_index=0,
+        ),
+        model.Token(
+            text=".", index_in_document=8, pos_tag=".", bio_tag="", sentence_index=0
+        ),
+    ]
 
     tokens2 = copy.deepcopy(tokens)
     tokens2.pop()
-    tokens2.append(model.Token(text="available", index_in_document=8,
-                               pos_tag="JJ", bio_tag="",
-                               sentence_index=0))
-    tokens2.append(model.Token(text=".", index_in_document=9,
-                               pos_tag=".", bio_tag="",
-                               sentence_index=0))
-    sentence2 = model.Sentence(tokens=tokens2)
+    tokens2.append(
+        model.Token(
+            text="available",
+            index_in_document=8,
+            pos_tag="JJ",
+            bio_tag="",
+            sentence_index=0,
+        )
+    )
+    tokens2.append(
+        model.Token(
+            text=".", index_in_document=9, pos_tag=".", bio_tag="", sentence_index=0
+        )
+    )
 
-    mention1 = model.Mention(ner_tag="Actor", sentence_index=0, token_indices=[2, 3])
-    mention2 = model.Mention(ner_tag="Further Specification", sentence_index=1, token_indices=[4, 5])
     doc = model.Document(
         text="I am the Head of the functional department.I am the Head of the functional department available.",
-        name="1", sentences=[sentence1, sentence2],
-        mentions=[mention1, mention2],
+        name="1",
+        sentences=[model.Sentence(tokens), model.Sentence(tokens2)],
+        mentions=[
+            model.Mention(ner_tag="Actor", sentence_index=0, token_indices=[2, 3]),
+            model.Mention(
+                ner_tag="Further Specification", sentence_index=1, token_indices=[4, 5]
+            ),
+        ],
         entities=[],
-        relations=[])
-    doc_sol = copy.deepcopy(doc)
-    doc_sol.sentences[0].tokens[6].text = "running"
-    doc_sol.sentences[1].tokens[6].text = "running"
+        relations=[],
+    )
 
-    doc_sol2 = copy.deepcopy(doc)
-    doc_sol2.sentences[0].tokens[3].text = "proletarian"
-    doc_sol2.sentences[1].tokens[3].text = "proletarian"
+    trafo = trafo101.Trafo101Step([doc], n=1)
+    aug = trafo.do_augment(doc)
 
-    doc_sol3 = copy.deepcopy(doc)
-    doc_sol3.sentences[0].tokens[6].text = "running"
-    doc_sol3.sentences[1].tokens[6].text = "functional"
-
-    # Act
-    doc_aug = trafo.do_augment(doc)
-    doc_aug2 = trafo2.do_augment(doc)
-    doc_aug3 = trafo3.do_augment(doc)
-
-    # Assert
-    assert doc_aug == doc_sol
-    assert doc_aug2 == doc_sol2
-    assert doc_aug3 == doc_sol3
+    print()
+    print(" ".join(t.text for t in doc.tokens))
+    print(" ".join(t.text for t in aug.tokens))
