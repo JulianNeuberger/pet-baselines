f32d009b109656011ed606ddc818b0223eb76760
Julian Neuberger <neuberger.julian@gmail.com>
2023-11-30 15:21:21 +0100

    fixes B.8

--- augment/trafo8.py
+++ augment/trafo8.py
@@ -1,6 +1,6 @@
 import typing
-from random import random
 
+from nltk import tokenize
 from transformers import FSMTForConditionalGeneration, FSMTTokenizer
 
 from augment import base, params
@@ -9,19 +9,16 @@ from transformations import tokenmanager
 
 
 # Author: Benedikt
-class Trafo8Step(base.AugmentationStep):
+class Trafo8Step(base.BaseTokenReplacementStep):
     def __init__(
         self,
         dataset: typing.List[model.Document],
-        p: float = 1,
-        max_outputs: int = 1,
+        n: int = 1,
         num_beams: int = 2,
         lang: str = "de",
     ):
-        super().__init__(dataset)
+        super().__init__(dataset, n)
         self.lang = lang
-        self.p = p
-        self.max_outputs = min(num_beams, max_outputs)
         self.num_beams = num_beams
         name_en_de = "facebook/wmt19-en-de"
         self.tokenizer_en_de = FSMTTokenizer.from_pretrained(name_en_de)
@@ -42,85 +39,43 @@ class Trafo8Step(base.AugmentationStep):
             params.IntegerParam(name="num_beams", min_value=1, max_value=20),
         ]
 
-    def do_augment(self, doc: model.Document):
+    def do_augment(self, doc: model.Document) -> model.Document:
         doc = doc.copy()
-        for sentence in doc.sentences:
-            i = 0
-            while i < len(sentence.tokens) - 1:
-                token = sentence.tokens[i]
-                current_bio = tokenmanager.get_bio_tag_short(token.bio_tag)
-                text_before = ""
-                j = 0
-                while (
-                    tokenmanager.get_bio_tag_short(sentence.tokens[i + j].bio_tag)
-                    == current_bio
-                ):
-                    if i + j < len(sentence.tokens) - 1:
-                        if j == 0:
-                            text_before += sentence.tokens[i + j].text
-                        else:
-                            text_before += " "
-                            text_before += sentence.tokens[i + j].text
-                    j += 1
-                    if i + j > len(sentence.tokens) - 1:
-                        break
 
-                if random() >= self.p:
-                    i += j
-                    continue
-                if text_before in [",", ".", "?", "!", ":", "#", "-"]:
-                    translated = text_before
-                else:
-                    translated = self.back_translate(text_before)
-                text_before_list = text_before.split()
-                translated_list = translated.split()
-                diff = len(translated_list) - len(text_before_list)
-                if diff > 0:
-                    token.text = translated_list[0]
-                    token.pos_tag = tokenmanager.get_pos_tag([token.text])[0]
-                    if len(translated_list) > 1:
-                        for k in range(1, len(translated_list)):
-                            tok = model.Token(
-                                text=translated_list[k],
-                                index_in_document=token.index_in_document + i + k,
-                                pos_tag=tokenmanager.get_pos_tag([token.text])[0],
-                                bio_tag=tokenmanager.get_continued_bio_tag(
-                                    token.bio_tag
-                                ),
-                                sentence_index=token.sentence_index,
-                            )
-                            tokenmanager.create_token(doc, tok, i + k)
-                elif diff == 0:
-                    for k in range(0, len(translated_list)):
-                        index_in_doc = token.index_in_document
-                        sentence.tokens[i + k].text = translated_list[k]
-                        sentence.tokens[i + k].pos_tag = tokenmanager.get_pos_tag(
-                            [token.text]
-                        )[0]
-                else:
-                    for k in range(len(translated_list)):
-                        sentence.tokens[i + k].text = translated_list[k]
-                        sentence.tokens[i + k].pos_tag = tokenmanager.get_pos_tag(
-                            [token.text]
-                        )[0]
-                    for k in range(len(translated_list), len(text_before_list)):
-                        tokenmanager.delete_token(
-                            doc,
-                            sentence.tokens[i + len(translated_list)].index_in_document,
-                        )
-                i = i + j + diff
+        candidates = self.get_replacement_candidates(doc)
+        num_changes = 0
+        for candidate in candidates:
+            text = " ".join(t.text for t in candidate)
+            if text in [",", ".", "?", "!", ":", "#", "-"]:
+                continue
+
+            translated = self.back_translate(text)
+            if translated is None:
+                continue
+
+            translated_tokens = tokenize.word_tokenize(translated)
+            tokenmanager.replace_sequence_text_in_sentence(
+                doc,
+                candidate[0].sentence_index,
+                candidate[0].index_in_sentence(doc),
+                candidate[-1].index_in_sentence(doc),
+                translated_tokens,
+            )
+
+            num_changes += 1
+            if num_changes == self.n:
+                break
+
         return doc
 
-    #  returns the back translated text, when it's not working, it returns the old text
-    def back_translate(self, en: str):
+    def back_translate(self, en: str) -> typing.Optional[str]:
         try:
             de = self.en2de(en)
-            en_new = self.de2en(de)
+            return self.de2en(de)
         except Exception as ex:
             print(ex)
             print("Returning Default due to Run Time Exception")
-            en_new = en
-        return en_new
+            return None
 
     def en2de(self, input):
         input_ids = self.tokenizer_en_de.encode(input, return_tensors="pt").to("cuda")
@@ -145,5 +100,3 @@ class Trafo8Step(base.AugmentationStep):
             # TODO: this should be able to return multiple sequences
             predicted_outputs.append(decoded)
         return predicted_outputs[0]
-
-
